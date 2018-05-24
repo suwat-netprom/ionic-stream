3582078af2a5aa055043672be33b67b85867dd6e
suwat <suwat_@outlook.com>
2018-05-24 12:08:09 +0700

    add test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Media, MediaObject } from '@ionic-native/media';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  file: MediaObject = this.media.create('http://radio182.bugaboo.tv/liveedge_goodtime_radio/smil:audio.smil/playlist.m3u8');
 
+  constructor(public navCtrl: NavController, private media: Media) {
+    this.file.play();
   }
+
+  play(){
+    console.log('play');
+    this.file.play();
+  }
+
+  pause(){
+    console.log('pause');
+    this.file.pause();
+  }
+
 
 }
